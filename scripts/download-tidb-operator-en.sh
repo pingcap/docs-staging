a6f41bb2a5a6cce41689ae8a9a3a5445204e8a47
Ran <huangran.alex@gmail.com>
2023-04-04 14:25:38 +0800

    add tidb operator release 1.5

--- scripts/download-tidb-operator-en.sh
+++ scripts/download-tidb-operator-en.sh
@@ -1,4 +1,6 @@
 yarn download:tidb-operator en
+yarn download:tidb-operator en release-1.5
+yarn download:tidb-operator en release-1.4
 yarn download:tidb-operator en release-1.3
 yarn download:tidb-operator en release-1.2
 yarn download:tidb-operator en release-1.1
