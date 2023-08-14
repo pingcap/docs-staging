e1ac0c206d287706d6278678525d3a51ba0bb1fb
Ran <huangran.alex@gmail.com>
2023-08-14 17:45:53 +0800

    update download script for v7.3 and v7.2

--- scripts/download-tidb-zh.sh
+++ scripts/download-tidb-zh.sh
@@ -1,4 +1,6 @@
 yarn download:tidb:zh
+yarn download:tidb:zh --ref release-7.3
+yarn download:tidb:zh --ref release-7.2
 yarn download:tidb:zh --ref release-7.1
 yarn download:tidb:zh --ref release-7.0
 yarn download:tidb:zh --ref release-6.6
