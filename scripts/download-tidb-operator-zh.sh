62ebbd91b938931dbf49450b92c7432e328f4ba9
Aolin <aolin.zhang@pingcap.com>
2024-03-21 11:01:22 +0800

    add tidb-operator release 1.6
    Signed-off-by: Aolin <aolin.zhang@pingcap.com>

--- scripts/download-tidb-operator-zh.sh
+++ scripts/download-tidb-operator-zh.sh
@@ -1,4 +1,5 @@
 yarn download:tidb-operator zh
+yarn download:tidb-operator zh release-1.6
 yarn download:tidb-operator zh release-1.5
 yarn download:tidb-operator zh release-1.4
 yarn download:tidb-operator zh release-1.3
