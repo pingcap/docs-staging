579e31a84d9f1dc15b65cc11a83a6f0dbb6eba4e
xixirangrang <hfxsd@hotmail.com>
2024-08-29 09:04:53 +0800

    Update download-tidb-en.sh (#88)

--- scripts/download-tidb-en.sh
+++ scripts/download-tidb-en.sh
@@ -1,4 +1,6 @@
 yarn download:tidb:en
+yarn download:tidb:en --ref release-8.3
+yarn download:tidb:en --ref release-8.2
 yarn download:tidb:en --ref release-8.1
 yarn download:tidb:en --ref release-8.0
 yarn download:tidb:en --ref release-7.6
