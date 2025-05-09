4d8035f46155a672c986d2a6ffb68243d507d204
Suhaha <jklopsdfw@gmail.com>
2025-05-09 09:43:29 +0800

    Add variable handling for zip files in download utilities
    - Introduced `getVariablesFromZip` and `variablesReplaceStream` functions in `variable.js` to extract and replace variables from zip files.
    - Updated `retrieveTiDBMDsFromZip` and `retrieveCloudMDsFromZip` functions to utilize the new variable handling, enhancing the processing of markdown files.

---
+++ packages/download/variable.js
@@ -0,0 +1,28 @@
+const replaceStream = require("replacestream");
+
+export const getVariablesFromZip = (zip, filePath) => {
+  let variables = {};
+
+  try {
+    const zipEntries = zip.getEntries();
+    const variablesEntry = zipEntries.find((entry) =>
+      entry.entryName.endsWith(filePath)
+    );
+    variables = JSON.parse(variablesEntry.getData().toString());
+    console.log(variables);
+  } catch {}
+
+  return variables;
+};
+
+function getValueByPath(obj, path) {
+  return path.split(".").reduce((acc, key) => (acc ? acc[key] : ""), obj) ?? "";
+}
+const variablePattern = /{{\s*\.(.+?)\s*}}/g;
+
+export const variablesReplaceStream = (variables) => {
+  return replaceStream(variablePattern, (match, path) => {
+    const value = getValueByPath(variables, path.trim());
+    return String(value);
+  });
+};
