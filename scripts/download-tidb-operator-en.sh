2f1b0a9c31c934940a3c5e087d2e59a534d1004b
Aolin <aolin.zhang@pingcap.com>
2025-11-27 11:04:12 +0800

    Update docs-tidb-operator dev branch from master to main

--- scripts/download-tidb-operator-en.sh
+++ scripts/download-tidb-operator-en.sh
@@ -1,4 +1,4 @@
-yarn download:tidb-operator en
+yarn download:tidb-operator en main
 yarn download:tidb-operator en release-2.0
 yarn download:tidb-operator en release-1.6
 yarn download:tidb-operator en release-1.5
